2ade87ef0a9a5f66dcfea13149f2f1ea251c829a
tomwey <tomwey@163.com>
2018-08-01 11:40:26 +0800

    ccc

--- src/pages/friend-group-list/friend-group-list.ts
+++ src/pages/friend-group-list/friend-group-list.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Utils } from '../../provider/Utils';
-// import { MCanvas } from '../../'
 
 /**
  * Generated class for the FriendGroupListPage page.
@@ -34,13 +33,6 @@ export class FriendGroupListPage {
   }
 
   groupAvatar(group) {
-    // let mc = new MCanvas({
-    //   width: 120,
-    //   height: 120,
-    //   backgroundColor: 'gray'
-    // });
-    // console.log(mc);
-
     return "assets/imgs/to-user.jpg";
   }
 
