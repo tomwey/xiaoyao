d7d8966f3cab47469871b0e438a51bb532f37ee7
tomwey <tomwey@163.com>
2018-08-01 18:31:38 +0800

    ccc

--- src/pages/recommend/recommend.ts
+++ src/pages/recommend/recommend.ts
@@ -15,17 +15,32 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class RecommendPage {
 
+  friends: any = [];
+  groups: any = [];
   constructor(public navCtrl: NavController, 
     private viewCtrl: ViewController,
     public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad RecommendPage');
+    // console.log('ionViewDidLoad RecommendPage');
+    setTimeout(() => {
+      this.loadFriends();
+      this.loadGroups();
+    }, 100);
   }
 
   close() {
     this.viewCtrl.dismiss();
   }
+
+  loadFriends() {
+
+  }
+
+  loadGroups() {
+    
+  }
+
 
 }
