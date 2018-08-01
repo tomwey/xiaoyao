434fd4fb867873f042a7dc1bc72fc294dc2615bd
tomwey <tomwey@163.com>
2018-08-01 21:26:20 +0800

    ccc

--- src/pages/recommend/recommend.ts
+++ src/pages/recommend/recommend.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Socials } from '../../provider/Socials';
+import { Utils } from '../../provider/Utils';
 
 /**
  * Generated class for the RecommendPage page.
@@ -17,9 +19,13 @@ export class RecommendPage {
 
   friends: any = [];
   groups: any = [];
+  userId: any;
+
   constructor(public navCtrl: NavController, 
     private viewCtrl: ViewController,
+    private socials: Socials,
     public navParams: NavParams) {
+      this.userId = Utils.getQueryString('uid');
   }
 
   ionViewDidLoad() {
@@ -30,16 +36,43 @@ export class RecommendPage {
     }, 100);
   }
 
+  groupAvatar(group) {
+    return "assets/imgs/to-user.jpg";
+  }
+
+  calcMemberCount(group) {
+    // console.log(group);
+    // console.log(this.groupData);
+    if (!group) return 0;
+    if (!group.data) return 0;
+
+    return group.data.length;
+    // let arr = this.groupData[group.id.toString()];
+    // return arr.length;
+  }
+
   close() {
     this.viewCtrl.dismiss();
   }
 
   loadFriends() {
-
+    this.socials.GetMyFriends()
+      .then(data => {
+        if (data && data['data']) {
+          this.friends = data['data'];
+        }
+      })
+      .catch(error => {});
   }
 
   loadGroups() {
-    
+    this.socials.GetGroupInfo()
+    .then(data => {
+      if (data && data['data']) {
+        this.groups = data['data'];
+      }
+    })
+    .catch(error => {});
   }
 
 
